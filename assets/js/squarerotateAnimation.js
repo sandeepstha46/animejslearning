ecc6e43edadbd65fedaa5af7a2aed96e0e05ab84
Sandeep Shrestha <sandeepstha64@gmail.com>
2025-05-07 14:55:06 +0545

    square rotate

---
+++ assets/js/squarerotateAnimation.js
@@ -0,0 +1,19 @@
+anime.waapi.animate('.waapi.square', {
+    x: '17rem',
+    rotate: 180,
+    loop: true,
+    alternate: true,
+});
+
+// JS Animation
+
+const data = { x: '0rem', rotate: '0deg' }
+const $log = document.querySelector('code');
+
+anime.animate(data, {
+    x: 17,
+    rotate: 180,
+    loop: true,
+    alternate: true,
+    onRender: () => $log.innerHTML = JSON.stringify(data)
+});
